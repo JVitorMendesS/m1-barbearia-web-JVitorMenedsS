5e21e0547707dab9de99ad9a1282d844a27bc0dc
João Mendes <jvitormendess@outlook.com>
2024-09-28 10:00:34 -0300

    Funções adicionadas
    estruturação de funções solicitadas para a tarefa

--- script.js
+++ script.js
@@ -19,27 +19,65 @@ const barbearia = {
   
   
 function buscaCortePorId(id) {
+    let objReturn = barbearia.cortes.find(obj => obj.id === id);
+
+    if (objReturn){
+        return objReturn;
+    } else {
+        return "Corte não encontrado";
+    }
 }
 
 function buscaBarbaPorId(id) {
+    let objReturn2 = barbearia.barbas.find(obj2 => obj2.id === id);
+
+    if (objReturn2){
+        return objReturn2;
+    } else {
+        return "Barba não encontrada";
+    }
 }
 
 function verificaStatusBarbearia() {
+    if (barbearia.estaAberto === true){
+        return "Estamos abertos";
+    } else{
+        return "Estamos fechados";
+    }
 }
 
 function retornaTodosCortes() {
+    return barbearia.cortes;
 }
 
 function retornaTodasBarbas() {
+    return barbearia.barbas;
 }
 
 function criaPedido(nomeCliente, corteId, barbaId) {
+    const corte = buscaCortePorId(corteId);
+    const barba = buscaBarbaPorId(barbaId);
+
+    return {
+        nome: nomeCliente,
+        pedidoCorte: corte.tipo,
+        pedidoCortePreco: corte.valor,
+        pedidoBarba: barba.tipo,
+        pedidoBarbaPreco: barba.valor
+    };
 }
 
 function atualizarServico(lista, id, valor, tipo) {
+    let item = lista.find(obj => obj.id === id);
+    if (item) {
+        item.valor = valor;
+        item.tipo = tipo;
+    }
+    return lista;
 }
 
 function calculaTotal(pedido) {
+    return pedido.pedidoCortePreco + pedido.pedidoBarbaPreco;
 }
   
   
